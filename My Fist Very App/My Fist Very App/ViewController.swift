c6093a3150d643ac55735194ed427622a62aedb7
vovanbechin <bechin.vovan@gmail.com>
2019-08-31 15:55:39 +0900

    Create App

---
+++ My Fist Very App/My Fist Very App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Fist Very App
+//
+//  Created by bechin vovan on 2019/08/31.
+//  Copyright © 2019 bechin vovan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
