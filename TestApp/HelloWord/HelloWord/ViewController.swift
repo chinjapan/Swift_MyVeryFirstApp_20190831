ad84e128e31998274278f526e6c3b4eb31523ec4
vovanbechin <bechin.vovan@gmail.com>
2019-09-01 15:17:42 +0900

    Helloword

---
+++ TestApp/HelloWord/HelloWord/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HelloWord
+//
+//  Created by bechin vovan on 2019/09/01.
+//  Copyright © 2019 bechinvovan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+       
+    }
+
+
+}
+
